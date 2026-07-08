0f95e90d1513bd178bd4c6ceb0cd7770e8f5f21b
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-07-09 03:52:02 +0500

    REFACTOR: update data access object for books_dao.g

--- lib/data/local/daos/books_dao.g.dart
+++ lib/data/local/daos/books_dao.g.dart
@@ -1,8 +1,5 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
 part of 'books_dao.dart';
 
-// ignore_for_file: type=lint
 mixin _$BooksDaoMixin on DatabaseAccessor<AppDatabase> {
   $BooksTable get books => attachedDatabase.books;
   $CategoriesTable get categories => attachedDatabase.categories;
