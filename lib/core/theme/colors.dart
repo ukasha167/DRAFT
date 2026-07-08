d0125643807fec50deef6deb81d6b888800edae7
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-07-09 03:52:03 +0500

    REFACTOR: update theme configuration in colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const Color draftBackground = Color(0xFFF7F3EE);
+const Color draftSurface = Color(0xFFFFFFFF);
+const Color draftSheetBg = Color(0xFFFFFFFF);
+
+const Color draftBorder = Color(0xFFE8E2D9);
+const Color draftDivider = Color(0xFFEFEBE4);
+
+const Color draftInk = Color(0xFF1A1410);
+const Color draftInkSecondary = Color(0xFF8B7E74);
+const Color draftInkDisabled = Color(0xFFBDB5AE);
+
+const Color draftRed = Color(0xFFB83232);
+const Color draftAmber = Color(0xFFC8A86B);
+
+const Color draftBackgroundDark = Color(0xFF1A1610);
+const Color draftSurfaceDark = Color(0xFF221E18);
+const Color draftBorderDark = Color(0xFF3A342C);
+const Color draftDividerDark = Color(0xFF2E2820);
+const Color draftInkDark = Color(0xFFF2EDE6);
+const Color draftInkSecondaryDark = Color(0xFF9E9188);
+const Color draftInkDisabledDark = Color(0xFF6B6158);
